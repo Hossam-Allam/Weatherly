a5b42e5c3fdb44851a3d1df4ba49bf8e1cbb9daf
hossam-allam <h.refaie@stu.khas.edu.tr>
2025-06-10 02:32:49 +0300

    new icons

---
+++ src/iconmap.js
@@ -0,0 +1,23 @@
+import snowIcon from "./icons/snow.png";
+import fogIcon from "./icons/fog.png";
+import rainIcon from "./icons/rain.png";
+import windIcon from "./icons/wind.jpg";
+import cloudyIcon from "./icons/cloud.png";
+import partlyCloudyDayIcon from "./icons/partly-cloudy-day.png";
+import partlyCloudyNightIcon from "./icons/partly-cloudy-night.png";
+import clearDayIcon from "./icons/clear-day.png";
+import clearNightIcon from "./icons/clear-night.png";
+import defaultIcon from "./icons/cloud.png";
+
+export const ICON_MAP = {
+  snow: snowIcon,
+  fog: fogIcon,
+  rain: rainIcon,
+  wind: windIcon,
+  cloudy: cloudyIcon,
+  "partly-cloudy-day": partlyCloudyDayIcon,
+  "partly-cloudy-night": partlyCloudyNightIcon,
+  "clear-day": clearDayIcon,
+  "clear-night": clearNightIcon,
+  default: defaultIcon,
+};
