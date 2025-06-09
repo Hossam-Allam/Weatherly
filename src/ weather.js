2c584b7319a6c5deb67a1f5879cca8667993d686
hossam-allam <h.refaie@stu.khas.edu.tr>
2025-06-10 02:34:00 +0300

    weather info for 'today'

--- src/ weather.js
+++ src/ weather.js
@@ -1,3 +1,9 @@
+import { ICON_MAP } from "./iconmap";
+
+const city = document.querySelector(".city");
+const description = document.querySelector(".description");
+const temp = document.querySelector(".temp");
+
 async function weather(query = null, unit = "metric") {
   let response = await fetch(
     "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" +
@@ -18,8 +24,24 @@ async function weather(query = null, unit = "metric") {
 
 export function printForecast({ data, unit }) {
   console.log(data);
+  const tempUnit = unit === "us" ? "°F" : "°C";
+  let today = data.days[0];
+  city.innerHTML = data.resolvedAddress;
+  description.innerHTML = today.description;
+  temp.innerHTML = `${today.temp}${tempUnit}`;
 
-  // const tempUnit = unit === "us" ? "°F" : "°C";
+  const iconName = today.icon;
+  const iconURL = ICON_MAP[iconName] || ICON_MAP["default"];
+
+  let iconEl = document.querySelector(".weather-icon");
+  if (!iconEl) {
+    iconEl = document.createElement("img");
+    iconEl.classList.add("weather-icon");
+    temp.insertAdjacentElement("afterend", iconEl);
+  }
+  iconEl.src = `${iconURL}`;
+  iconEl.alt = iconName;
+
   // const speedUnit = unit === "us" ? "mph" : "km/h";
 
   // data.days.slice(0, 5).forEach((day, idx) => {
